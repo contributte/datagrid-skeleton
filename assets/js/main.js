140db57b2d2b19a30412fdd1b10e718fe3f58dc6
Tetiana Kobryn <kobryn@logmanager.cz>
2022-12-21 11:15:05 +0200

    added some scripts as modules

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
-// import '~/bootstrap/dist/js/bootstrap.min.js';
-// import '~/ublaboo-datagrid/assets/datagrid.js';
-// import '~/bootstrap-datepicker/dist/js/bootstrap-datepicker.js';
-// import '~/bootstrap/dist/js/bootstrap.bundle.js';
+import '~/ublaboo-datagrid/assets/datagrid.js';
+import '~/ublaboo-datagrid/assets/datagrid-spinners.js';
+import '~/ublaboo-datagrid/assets/datagrid-instant-url-refresh.js';
+import '~/nette.ajax.js/nette.ajax.js';
+import '~/jquery-ui-sortable/jquery-ui.min.js';
 
-import './../css/main.css'
+
+import './../css/main.css';
