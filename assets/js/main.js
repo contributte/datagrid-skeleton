42d4c5c7b3dfc0e3cde16e5a4db7fd7345057cb8
Milan Felix Šulc <rkfelix@gmail.com>
2025-03-11 21:04:15 +0100

    Assets: minimize to datagrid only

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,14 +22,6 @@ import { NajaAjax } from "../../vendor/ublaboo/datagrid/assets/ajax";
 import Select from "tom-select";
 import { Dropdown } from "bootstrap";
 
-// Code highlighting
-import Prism from "prismjs/components/prism-core";
-import "prismjs/components/prism-markup-templating";
-import "prismjs/components/prism-php";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-javascript";
-import "prismjs/components/prism-css";
-
 // Styles
 import '../css/main.css';
 
@@ -60,13 +52,5 @@ document.addEventListener("DOMContentLoaded", () => {
 				new TreeViewPlugin(),
 			],
 		},
-	});
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-	// Highlighting
-	const codes = document.querySelectorAll('code');
-	codes.forEach(code => {
-		Prism.highlightElement(code);
 	});
 });
